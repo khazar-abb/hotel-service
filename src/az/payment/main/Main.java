b6ac65e69d9bd8da19a763432824b036bdcfb33e
khazarbaghirov <khazar.baghirov@abb-bank.az>
2026-02-03 09:31:53 +0400

    🧩 Payment & Transaction System – Task 1

--- src/az/payment/main/Main.java
+++ src/az/payment/main/Main.java
@@ -1,4 +1,17 @@
 package az.payment.main;
 
+import az.payment.service.PaymentService;
+
 public class Main {
+    static void main() {
+        PaymentService paymentService = new PaymentService();
+
+        paymentService.makePayments("Book purchase", 50.00);
+        paymentService.makePayments("Course fee", 120.00);
+        paymentService.makePayments("Subscription", 30.00);
+
+        paymentService.listTransactions();
+
+        System.out.println("Total amount: " + paymentService.calculateTotal());
+    }
 }
--- src/az/payment/service/PaymentService.java
+++ src/az/payment/service/PaymentService.java
@@ -1,4 +1,42 @@
 package az.payment.service;
 
 public class PaymentService {
+    final int MAX_LIMIT = 10;
+
+    private String[] transactions = new String[MAX_LIMIT];
+    private double[] amounts = new double[MAX_LIMIT];
+    private int count;
+
+    public void makePayments(String description, double amount) {
+        if (count <= 10) {
+            for (int i = 0; i < transactions.length; i++) {
+                if (transactions[i] == null && amounts[i] == 0) {
+                    transactions[i] = description;
+                    amounts[i] = amount;
+                    count++;
+                    break;
+                }
+            }
+        } else {
+            System.out.println("Cannot make payment. Reached a limit: " + MAX_LIMIT);
+        }
+    }
+
+    public void listTransactions() {
+        if (count == 0) {
+            System.out.println("No transaction found.");
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println("TR (" + (i + 1) + ") - " + transactions[i] + " : " + amounts[i]);
+        }
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (int i = 0; i < count; i++) {
+            total += amounts[i];
+        }
+        return total;
+    }
 }
