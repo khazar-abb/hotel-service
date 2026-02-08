da4cf0eeabac529cad85771a8be1158016cab7ab
khazarbaghirov <khazar.baghirov@abb-bank.az>
2026-02-08 14:09:16 +0400

    Poly

--- src/AbstractionInheritancePolymorphism/Polymorphism/Main.java
+++ src/AbstractionInheritancePolymorphism/Polymorphism/Main.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 public class Main {
     static void main() {
 
-        ArrayList<Course>courses = new ArrayList<>();
+        ArrayList<Course> courses = new ArrayList<>();
 
         courses.add(new VideoCourse());
         courses.add(new LiveClass());
         courses.add(new InteractiveTest());
 
-        for (Course course : courses){
+        for (Course course : courses) {
             course.start();
         }
     }
